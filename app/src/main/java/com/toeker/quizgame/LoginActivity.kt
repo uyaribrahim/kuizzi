b1c49ff2283368c29c19a25d1b6262bb35fa4d7c
uyaribrahim <iuyar99@gmail.com>
2021-03-10 09:48:26 +0300

    initial commit

---
+++ app/src/main/java/com/toeker/quizgame/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.toeker.quizgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+
+
+    }
+}

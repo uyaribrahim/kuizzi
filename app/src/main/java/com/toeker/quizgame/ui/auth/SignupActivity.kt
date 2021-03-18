1644e3f6ca9a7b2778ebd696dcbae268ee2a189f
uyaribrahim <iuyar99@gmail.com>
2021-03-18 16:25:49 +0300

    Registration screen UI design

---
+++ app/src/main/java/com/toeker/quizgame/ui/auth/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.toeker.quizgame.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.toeker.quizgame.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}

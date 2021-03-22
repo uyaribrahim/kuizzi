0e96ef1a444289cbbd58f70f06072c2f796a96e9
uyaribrahim <iuyar99@gmail.com>
2021-03-23 00:06:45 +0300

    Add the Home Activity to Manifest.xml, change color, new id for login textview,  setOnClickListener for buttons in LoginActivity

--- app/src/main/java/com/toeker/quizgame/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/toeker/quizgame/ui/auth/LoginActivity.kt
@@ -1,8 +1,12 @@
 package com.toeker.quizgame.ui.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.toeker.quizgame.R
+import com.toeker.quizgame.ui.home.HomeActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -10,6 +14,16 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+
+        btnLogin.setOnClickListener{
+            val intent = Intent(this, HomeActivity::class.java).apply {  }
+            startActivity(intent)
+
+        }
+        signUpText.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java).apply {  }
+            startActivity(intent)
+        }
 
 
 
